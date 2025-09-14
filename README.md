5e317f8a070e255d4f733cdd6aa62d887ecaacd2
JayG / James Y <jayg@jayg.top>
2025-09-13 19:10:05 -0700

    Added booth endpoints, connected MongoDB, and added swagger docs

---
+++ README.md
@@ -0,0 +1,7 @@
+SETUP INSTRUCTIONS IN CONSTRUCTION
+
+How to setup:
+
+1. Run "npm install"
+2. Install MongoDB Community (https://www.mongodb.com/docs/manual/tutorial/install-mongodb-community-with-docker/)
+3. install mongosh (https://www.mongodb.com/docs/mongodb-shell/install/)
--- server/api/db/database.js
+++ server/api/db/database.js
@@ -1,7 +1,7 @@
 require("dotenv").config(); // Getting settings from .env file
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
+async function connectDB(){
     const mongoURI = process.env.MONGODB_URI;
     try {
         await mongoose.connect(mongoURI);
@@ -12,4 +12,46 @@ const connectDB = async () => {
     }
 };
 
-module.exports = connectDB;
+// Booth Schema
+
+const boothSchema = new mongoose.Schema({
+    id: { type: Number, required: true, unique: true },
+    name: { type: String, required: true, default: "Unnamed Booth" },
+    description: { type: String, required: true, default: "No description provided." },
+    location: { type: Array, required: true, default: [{x: 0, y: 0}] },
+    boothRunners: { type: Array, required: true, default: [] },
+    boothImage: { type: String, required: true, default: "https://pub-8f769214a6e4406c9614b19c1746938b.r2.dev/example.png" },
+    boothCategory: { type: Number, required: true, default: "0" }, // 0 - Community, 1 - Food, 2 - Activities, 3 - Popups
+    organization: { type: String, required: false, default: "Unspecified" },
+    menu: { type: Array, required: false, default: [{
+        "Example Item": 0.00,
+        "Another Example Item": 1.00,
+    }] },
+    activities: {
+        intendedAges: { type: Array, required: false, default: [0, 99] },
+        queue: { type: Number, required: false, default: 0 },
+        waitPerPerson: { type: Number, required: false, default: 0 }, // In minutes
+    },
+    popups: {
+        startTime: { type: Date, required: false, default: Date.now },
+        endTime: { type: Date, required: false, default: Date.now },
+    },
+});
+
+// Volunteer Schema
+
+const volunteerSchema = new mongoose.Schema({
+    id: { type: Number, required: true, unique: true },
+    name: { type: String, required: true, default: "Unnamed" },
+    jobs: {
+        isAssignedToBooth: { type: Boolean, required: false, default: false },
+        boothID: { type: Number, required: false, default: null },
+        jobDescription: { type: String, required: false, default: "Unassigned" },
+    },
+});
+
+module.exports = {
+    connectDB,
+    Booth: mongoose.model("Booth", boothSchema),
+    Volunteer: mongoose.model("Volunteer", volunteerSchema),
+};
