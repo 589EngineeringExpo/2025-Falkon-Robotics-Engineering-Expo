c5a7203888c83d614128ec2fbe640a9857f59ba9
WheatSeed <142546720+ISeed10I@users.noreply.github.com>
2025-09-17 17:19:02 +0000

    Initial commit to react branch

---
+++ react/my-app/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Header from './Header.js';
+
+
+
+function App() {
+    return (
+        <div>
+            <Header />
+            {/* Other components or content */}
+        </div>
+    );
+}
+
+export default App;
